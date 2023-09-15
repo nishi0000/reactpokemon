d71d5c86c398525d0bff522209110dc789edc70b
nishi0000 <142460496+nishi0000@users.noreply.github.com>
2023-09-15 15:28:44 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,46 +1,108 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { getAllPokemon } from "./utils/pokemon";
-
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
-  const [loading,setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchPokemonData = async () => {
+
+  // const test = () => {
+  //   return new Promise((resolve)=>{
+  //     return resolve("test");
+  //   })
+  // }
+
+  // const test2 = () => {
+  //   return new Promise((resolve)=>{
+  //     return resolve("test2");
+  //   })
+  // }
+
+  // const test3 = () => {
+  //   return new Promise((resolve)=>{
+  //     return resolve("test3");
+  //   })
+  // }
+
+  // const alltest = async() => {
+  //   const a = await test();
+  //   console.log(a);
+  //   const b = await test2();
+  //   console.log(b);
+  //   const c = await test3();
+  //   console.log(c);
+  // }
+  // const alltest = async() => {
+  //   const a = await Promise.all([
+  //     test(),
+  //     test2(),
+  //     test3()
+  //   ])
+  //   console.log(a);
+  // }
+
+  // alltest();
+
+  // const alwaysLateBoy = (ms) => {
+  //   new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, ms);
+  //   }).then(() => {
+  //     console.log(`すまん！${ms}ms待たせたな。`);
+  //   });
+  // };
+
+  //上記をawait使って書くと
+
+  const alwaysLateBoy = (ms) => {
+    
+  }
+  
+  
+
+  useEffect(() => {//fetchPokemonDataっていう関数を作って
+    const fetchPokemonData = async () => {//asyncを適用してawaitを使えるようにして
       //全てのポケモンデータを取得
-      let res = await getAllPokemon(initialURL);
+      let res = await getAllPokemon(initialURL);//ここでデータと一緒にresolveが帰ってくるからawaitで制御して
       //各ポケモンの詳細なデータを取得
-      loadPokemon(res.results);
-      // console.log(res.results);
+      loadPokemon(res.results);//ここで引数としてurl含んだデータを渡してる
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  const loadPokemon = (data) => {
-    let _pokemonData = Promise.all(
-      data.map((pokemon) =>{
-        console.log(pokemon)
+  const loadPokemon = (data) => {//ここでurl含んだデータを渡されてる
+    let _pokemonData = Promise.all(//promiseallは配列で関数を渡す、ここではmap関数（配列を返す関数）を使ってるからこれでいいってこと？か？
+      data.map((pokemon) => {//一個ずつ取り出してる
+        // console.log(pokemon)
+        let pokemonRecord = getPokemon(pokemon.url);//ここで引数としてurlを渡して、
+        return pokemonRecord;
       })
-    )
-  }
 
-  return <div className="App">
-    {loading ? (
-      <h1>ロード中・・・</h1>
-    ) : (
-      <>
-      <h1>ポケモンデータを取得しました。</h1>
-      </>
-    )
-  }
+      //だからこれは言うなれば
+      // a = await getPokemon(pokemon1url);
+      // b = await getPokemon(pokemon2url);
+      // ...
+      // みたいな話ってことよな
 
+    );
 
+    // console.log(_pokemonData); プロミスでラップされた？データが出力されている
+  };
 
-
-  </div>;
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <h1>ポケモンデータを取得しました。</h1>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/utils/pokemon.jsx
+++ src/utils/pokemon.jsx
@@ -1,7 +1,19 @@
+//ここでプロミス関数を作って、URL先のデータを取ってきて、resolveとdataを一緒に返してる（から、awaitが使える）
 export const getAllPokemon = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then((res) => res.json())
       .then((data) => resolve(data));
+  });
+};
+
+export const getPokemon = (url) => {//ポケモンの詳細なデータが入ったurlを引数としてもらってる
+  return new Promise((resolve) => {//ここで新しくプロミス関数を作って
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        resolve(data);//詳細データをresolveと一緒にreturnしてる
+        console.log(data);
+      });
   });
 };
